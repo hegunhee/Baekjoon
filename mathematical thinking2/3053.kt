a2f6ae9f6a6418d8e4d293b4bf7b64753ab63a1d
hegunhee <leech9876@naver.com>
2021-11-08 17:03:48 +0900

    math

---
+++ mathematical thinking2/3053.kt
@@ -0,0 +1,8 @@
+import java.util.*;
+import kotlin.math.*
+fun main(args :Array<String>){
+    val scan = Scanner(System.`in`)
+    val r = scan.nextInt().toDouble()
+    println("${(r*r)*PI}")
+    println("${((r*r)*2).toDouble()}")
+}
