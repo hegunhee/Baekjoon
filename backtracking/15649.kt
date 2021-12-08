5104507bab355be23f94531d592be84f2b390f9a
hegunhee <leech9876@naver.com>
2021-12-08 23:08:06 +0900

    backtracking1

---
+++ backtracking/15649.kt
@@ -0,0 +1,28 @@
+import java.io.BufferedWriter
+import java.io.OutputStreamWriter
+import java.util.*
+val bw = BufferedWriter(OutputStreamWriter(System.`out`))
+fun main(){
+    val temp = Scanner(System.`in`).nextLine().split(" ")
+    val arr = MutableList<Int>(temp[0].toInt(),{i -> i+1})
+    dfs(temp[1].toInt(),arr,mutableListOf<Int>())
+    bw.flush()
+
+}
+fun dfs(n : Int,dict : MutableList<Int>,arr:MutableList<Int>){
+    if(arr.size == n){
+        for(i in 0 until arr.size){
+            bw.write("${arr[i]} ")
+        }
+        bw.write("\n")
+        return
+    }
+    for(i in 0 until dict.size){
+        if(is_available(dict[i],arr)){
+            arr.add(dict[i])
+            dfs(n,dict,arr)
+            arr.removeAt(arr.size-1)
+        }
+    }
+}
+fun is_available(current_value : Int,arr : MutableList<Int>) = current_value !in arr
